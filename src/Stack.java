ec1eec27dc536000db90b4155c6d15908658c155
jhochsteger <jonas.hochsteger.04@gmail.com>
2020-12-11 12:37:37 +0100

    Added the Push Method to the Stack Class and made the Class Generic

--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,6 @@
-public class Stack {
+public class Stack <T>{
     private int stackLength;
+    private T[] stack;
 
     public Stack() {
 
@@ -7,5 +8,17 @@ public class Stack {
 
     public Stack(int stackLength) {
         this.stackLength = stackLength;
+        stack = (T[]) new Object[stackLength];
+    }
+
+    public void push(T newElement) throws StackFullException{
+        stack[findFirstNull()] = newElement;
+    }
+
+    public int findFirstNull() throws StackFullException{
+        for (int i = 0; i < stack.length; i++) {
+            if (stack[i] == null) return i;
+        }
+        throw new StackFullException("The Stack is already full");
     }
 }
